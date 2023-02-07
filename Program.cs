f8660c2636b52c6f21a21abb4f2efafb4f7ef0bc
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2023-02-07 23:03:50 +0700

    ง(- -)ง

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main (string[] args)
+    {
+        double C0X = 0, C0Y = 0, C0P0X = 0, C0P0Y = 0, n = 0;
+        C0X = double.Parse(Console.ReadLine());
+        C0Y = double.Parse(Console.ReadLine());
+        C0P0X = double.Parse(Console.ReadLine());
+        C0P0Y = double.Parse(Console.ReadLine());
+        n = double.Parse(Console.ReadLine());
+    }
+    static double Diameter (double cx, double cy, double px, double py)
+    {
+        return Math.Sqrt(Math.Pow((cx-px),2)+Math.Pow((cy-py),2));
+    }
+}
